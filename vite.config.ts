c337cfe351ba961ee3b7cf4edde991e4ebb1fe1e
Szymon <szymon.osinski.2009@gmail.com>
2023-07-06 17:44:56 +0200

    sdhdfhdf

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 const environment =
   process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 const apiUrls = {
-  production: "huj",
+  production: "https://example-api.com",
   development: "http://localhost:8888"
 };
 
