b8c836f45ab494daca0f1c94eddb4344e46a5607
Szymon <szymon.osinski.2009@gmail.com>
2023-07-06 17:33:24 +0200

    shsdhd

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 const environment =
   process.env.NODE_ENV === 'production' ? 'production' : 'development';
@@ -7,12 +7,19 @@ const environment =
 const apiUrls = {
   production: "huj",
   development: "http://localhost:8888"
-}
+};
+
+const processShim = {
+  env: {
+    NODE_ENV: environment,
+    API_URL: apiUrls[environment]
+  }
+};
 
 export default defineConfig({
   base: '/vite-deploy/',
   plugins: [react()],
   define: {
-    'process.env.API_URL': JSON.stringify(apiUrls[environment]),
+    'process': JSON.stringify(processShim)
   }
-})
+});
